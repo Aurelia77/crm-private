e5bb52043ff0341454cfc5c5fb1264e2a0b22e7c
Aurelia H <aurelia.h@hotmail.fr>
2024-01-17 12:08:07 +0100

    Mise en commentaire bouton ajout 200 contacts

--- app/Components/Admin.tsx
+++ app/Components/Admin.tsx
@@ -398,7 +398,7 @@ export default function Admin({ currentUserId }: AdminType) {
         <Box sx={{ display: "flex", justifyContent: "space-around", marginBottom: "20px" }} >
           <Button variant="contained" color='success' onClick={() => addCategoriesOnFirebaseAndReload(currentUserId)}>1-Ajouter Catégories</Button>
           <Button variant="contained" color='ochre' onClick={() => addFakeDataWithCat(currentUserId)}>2-Ajouter Contacts Test (x7)</Button>
-          <Button variant="contained" color='warning' onClick={() => addLaurianeDataWithCat(currentUserId)}>Ajouter Contacts LAURIANE (x 201)</Button>
+          {/* <Button variant="contained" color='warning' onClick={() => addLaurianeDataWithCat(currentUserId)}>Ajouter Contacts LAURIANE (x 201)</Button> */}
             {/*   <Button variant="contained" onClick={handleSaveAll}>Sauvegarder TOUTES mes données</Button> */}
         </Box>
         <Box sx={{ display: "flex", justifyContent: "space-around", }} >
