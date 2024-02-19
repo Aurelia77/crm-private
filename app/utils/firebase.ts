b55009a6a51e75aaaac3a642d5e4bf1851e13784
Aurelia H <aurelia.h@hotmail.fr>
2024-02-19 18:07:15 +0100

    Petites améliorations

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -12,33 +12,17 @@ import laurianeDatax54 from '../data/contactsLaurianex50'
 import { contactCategories, emptyContact } from './toolbox'
 import contactsLaurianeNameAndCatLabel from '../data/contactsLaurianeNomEtCat'
 
-
-
-// // LAURIANE Seulement
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY_L,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN_L,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   //databaseURL: "???", // Realtime Database ?
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_L,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_L,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID_L,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID_L,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID_L // Google Analytics
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID // Google Analytics
   //databaseURL: "https://crm-lauriane-c8084-default-rtdb.europe-west1.firebasedatabase.app", 
 };
-
-// // Les autres
-// const firebaseConfig = {  
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   //databaseURL: "???", // Realtime Database ?
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID // Google Analytics
-// };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
